23667275752710bdf73f32207a7b840b608de7f1
josepedrolorenzini <webmaster@marbleceramiccorp.com.au>
2026-01-28 21:36:33 +1100

    Remove .ts extension from imports

--- app/page.tsx
+++ app/page.tsx
@@ -2,8 +2,9 @@ import AcmeLogo from '@/app/ui/acme-logo';
 import { ArrowRightIcon } from '@heroicons/react/24/outline';
 import Link from 'next/link';
 import styles from '@/app/ui/home.module.css';
-import { lusitana } from '@/app/ui/fonts';
+import { Lusitana } from 'next/font/google';
 import Image from 'next/image';
+import { lusitana } from './ui/fonts';
 
 
 export default function Page() {
